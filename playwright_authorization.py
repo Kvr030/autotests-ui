80640b84abd7102324a542eda51db5055ee1e06c
Kvr030 <kostik1711@yandex.ru>
2025-10-25 17:24:06 +0300

    первый тест1

--- playwright_authorization.py
+++ playwright_authorization.py
@@ -2,5 +2,21 @@ from playwright.sync_api import sync_playwright, expect
 
 
 with sync_playwright() as playwright:
-    chromium = playwright.chromium.launch()
-    
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+
+    email_input = page.locator('//input[@id=":r0:"]')
+    email_input.fill("user.name@gmail.com")
+
+    password_input = page.locator('//input[@id=":r1:"]')
+    password_input.fill("password")
+
+    page.locator('//button[@id="login-page-login-button"]').click()
+
+    wrong_email = page.locator('//div[@data-testid="login-page-wrong-email-or-password-alert"]')
+    expect(wrong_email).to_be_visible()
+    expect(wrong_email).to_have_text('Wrong email or password')
+
+    page.wait_for_timeout(2000)
