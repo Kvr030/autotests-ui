57d327364dcf6bb1e86cd6c43b6e2ee8ee318bf3
Kvr030 <kostik1711@yandex.ru>
2025-10-27 18:45:47 +0300

    фикстуры

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,58 +1,23 @@
-from playwright.sync_api import sync_playwright, expect
 import pytest
+from playwright.sync_api import expect, Page
 
 
+@pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list():
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = context.new_page()
+def test_empty_courses_list(chromium_page_with_state: Page):
+    chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
-        # Открыть страницу https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-        # Заполнить форму регистрации и нажать на кнопку "Registration"
-        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        email_input.fill("user@gmail.com")
+    courses_title = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
+    expect(courses_title).to_be_visible()
+    expect(courses_title).to_have_text('Courses')
 
-        username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-        username_input.fill("username")
+    empty_view_icon = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
+    expect(empty_view_icon).to_be_visible()
 
-        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-        password_input.fill("password")
+    empty_view_title = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
+    expect(empty_view_title).to_be_visible()
+    expect(empty_view_title).to_have_text('There is no results')
 
-        page.get_by_test_id('registration-page-registration-button').click()
-
-        # Сохранить состояние браузера
-        context.storage_state(path='./help/browser-state.json')
-
-        # Создать новую сессию браузера. В контекст необходимо подставить сохраненное состояние
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context(storage_state='./help/browser-state.json')
-        page = context.new_page()
-
-        # Открыть страницу https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses. Страница "Courses" должна открыться без авторизации
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
-
-        # Проверить наличие и текст заголовка "Courses"
-        title = page.get_by_test_id('courses-list-toolbar-title-text')
-        expect(title).to_be_visible()
-        expect(title).to_have_text("Courses")
-
-        # Проверить наличие и текст блока "There is no results"
-        info = page.get_by_test_id('courses-list-empty-view-title-text')
-        expect(info).to_be_visible()
-        expect(info).to_have_text("There is no results")
-
-        # Проверить наличие и видимость иконки пустого блока
-        empty_block = page.get_by_test_id('courses-list-empty-view-icon')
-        expect(info).to_be_visible()
-
-        # Проверить наличие и текст описания блока: "Results from the load test pipeline will be displayed here"
-        info = page.get_by_test_id('courses-list-empty-view-description-text')
-        expect(info).to_be_visible()
-        expect(info).to_have_text("Results from the load test pipeline will be displayed here")
-
-        # Просто посмотреть на страницу, так как все выполняется очень быстро
-        page.wait_for_timeout(2000)
+    empty_view_description = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
+    expect(empty_view_description).to_be_visible()
+    expect(empty_view_description).to_have_text('Results from the load test pipeline will be displayed here')
