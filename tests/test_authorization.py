fa457e84f07a706c38b5a85575f7070c23b5105e
Kvr030 <kostik1711@yandex.ru>
2025-10-28 09:53:46 +0300

    параметризация

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -4,25 +4,29 @@ from playwright.sync_api import expect, Page
 
 @pytest.mark.regression
 @pytest.mark.authorization
-def test_authorisation(chromium_page: Page):
-    # Необходимо написать скрипт, который откроет страницу https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login
+@pytest.mark.parametrize(
+    "email, password",
+    [
+        ("user.name@gmail.com", "password"),
+        ("user.name@gmail.com", "  "),
+        ("  ", "password"),
+    ]
+)
+def test_wrong_email_or_password_authorization(chromium_page: Page, email: str, password: str):
     chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
-    # Заполнит поле "Email" значением user.name@gmail.com
     email_input = chromium_page.get_by_test_id('login-form-email-input').locator('input')
-    email_input.fill("user.name@gmail.com")
+    email_input.fill(email)
 
-    # Заполнит поле "Password" значением password
     password_input = chromium_page.get_by_test_id('login-form-password-input').locator('input')
-    password_input.fill("password")
+    password_input.fill(password)
 
-    # Нажмет на кнопку "Login"
-    chromium_page.get_by_test_id('login-page-login-button').click()
+    login_button = chromium_page.get_by_test_id('login-page-login-button')
+    login_button.click()
 
-    # Проверит наличие алерта с текстом "Wrong email or password"
-    wrong_email = chromium_page.get_by_test_id('login-page-wrong-email-or-password-alert')
-    expect(wrong_email).to_be_visible()
-    expect(wrong_email).to_have_text('Wrong email or password')
+    wrong_email_or_password_alert = chromium_page.get_by_test_id('login-page-wrong-email-or-password-alert')
+    expect(wrong_email_or_password_alert).to_be_visible()
+    expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
 
-    # Просто посмотреть на страницу, так как все выполняется очень быстро
-    chromium_page.wait_for_timeout(3000)
+    #Зрительная проверка работы теста
+    chromium_page.wait_for_timeout(2000)
